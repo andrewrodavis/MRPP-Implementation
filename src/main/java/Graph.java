f2d2fa4cf0848588d7dd6c7eb71de6d377624add
Andrew <andrewrodavis@gmail.com>
2020-02-09 18:18:20 -0500

    File started. Need to actually code

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -1,2 +1,42 @@
+import java.util.ArrayList;
+
+/**
+ * Class: Graph
+ *
+ * Maintains a list of nodes that make up the entire graph
+ */
 public class Graph {
+    // The graph to be maintained and used
+    ArrayList<Node> graph = new ArrayList<Node>();
+
+    // Size of the graph --> Number of nodes
+    int numNodes;
+
+    /**
+     * Function: initGraph
+     *
+     * Initializes the graph by
+     *      Filling this.graph with a node, each of which maintains its own neighbors list and weights
+     *      (Maybe, depending on implementation) Initializes arc strength of each edge
+     */
+    public void initGraph(){
+
+    }
+
+    public void getNode(String node){
+
+    }
+
+    /**
+     * Function: displayGraphVertices
+     *
+     * When called, shows the graph in the following format:
+     * Vertex  |  Neighbor (weight)
+     *
+     * @implNote Maybe put this in test
+     */
+    public void displayGraphVertices(){
+
+    }
+
 }
